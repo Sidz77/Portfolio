bfafba84e424bcea92e3dcf992a62e6d25846ded
Sidz77 <sudarshanbhosale7777@gmail.com>
2026-02-20 00:23:22 +0530

    Updated portfolio UI, avatar and projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["buildings MVPs", "managing projects", "frontend development", "UI/UX", "backend development"],
+    strings: ["Frontend development", "Backend development","Managing projects","Competitive programming"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
